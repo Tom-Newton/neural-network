9c52fa3f12915cfc4d89e454a2ec7f27b134a499
Thomas Newton <thomas.w.newton@gmail.com>
2020-02-24 20:47:18 +0000

    Create Network class

--- network.py
+++ network.py
@@ -2,41 +2,44 @@ import numpy as np
 import single_neuron
 
 
-def get_derivatives(network):
-    derivatives = []
-    for layer in network:
-        derivatives.append([lambda y, X_tilde: 0]*len(layer))
+class Network:
+    def __init__(self, network_data):
+        self.data = network_data
 
-    # stem is a function of y
-    # derivatives are functions of y and X_tilde
-    # y is relervant from the first stem. X_tilde only becomes relervant at the end
+    def get_derivatives(self):
+        derivatives = []
+        for layer in self.data:
+            derivatives.append([lambda y, X_tilde: 0]*len(layer))
 
-    def differentiate(network, stem=lambda y: (y - network[0][0].output()), i=0, j=0):
-        neuron = network[i][j]
-        derivatives[i][j] = lambda y, X_tilde: derivatives[i][j](
-            y, X_tilde) + np.matmul(X_tilde.T, stem(y))
+        # stem is a function of y
+        # derivatives are functions of y and X_tilde
+        # y is relervant from the first stem. X_tilde only becomes relervant at the end
 
-        for input_location in neuron.input_locations:
-            if type(input_location) == tuple:
-                def new_stem(y):
-                    output = network[input_location[0]
-                                     ][input_location[1]].output
-                    return stem(y)*neuron.w[input_location[1] + 1]*output*(np.ones(y.shape) - output)
+        def differentiate(network_data, stem=lambda y: (y - network_data[0][0].output), i=0, j=0):
+            neuron = network_data[i][j]
+            derivatives[i][j] = lambda y, X_tilde: derivatives[i][j](
+                y, X_tilde) + np.matmul(X_tilde.T, stem(y))
 
-                differentiate(network, new_stem,
-                              input_location[0], input_location[1])
-    differentiate(network)
-    return derivatives
+            for input_location in neuron.input_locations:
+                if type(input_location) == tuple:
+                    def new_stem(y):
+                        output = network_data[input_location[0]
+                                         ][input_location[1]].output
+                        return stem(y)*neuron.w[input_location[1] + 1]*output*(np.ones(y.shape) - output)
 
+                    differentiate(network_data, new_stem,
+                                  input_location[0], input_location[1])
+        differentiate(self.data)
+        return derivatives
 
-def update_network(network, X):
-    for layer in network[::-1]:
-        # Iterates layers in reverse order
-        for neuron in layer:
-            neuron.update_X_tilde(network, X)
-            neuron.update_output()
-    return network[0][0].output
+    def update_network(self, X):
+        for layer in self.data[::-1]:
+            # Iterates layers in reverse order
+            for neuron in layer:
+                neuron.update_X_tilde(self.data, X)
+                neuron.update_output()
+        return self.data[0][0].output
 
-def output(network):
-    # Assumes the network has been updated
-    return network[0][0].output
+    def output(self):
+        # Assumes the network has been updated
+        return self.data[0][0].output
--- test_network.py
+++ test_network.py
@@ -1,52 +1,53 @@
 import unittest
 import numpy as np
-import network
+from network import Network
 from single_neuron import SingleNeuron, predict
 
 
 class Tests(unittest.TestCase):
     def setUp(self):
-        self.network = [[SingleNeuron([(1, 0), (1, 1)])],
-                        [SingleNeuron([(2, 0), (2, 1)]),
-                         SingleNeuron([(2, 0), (2, 1)])],
-                        [SingleNeuron([0, 1]), SingleNeuron([0, 1])]]
+        self.network = Network([[SingleNeuron([(1, 0), (1, 1)])],
+                                [SingleNeuron([(2, 0), (2, 1)]),
+                                 SingleNeuron([(2, 0), (2, 1)])],
+                                [SingleNeuron([0, 1]), SingleNeuron([0, 1])]])
 
         self.X = np.array([[1, 3],
                            [2, 4]])
 
     def test_get_differentials(self):
-        network.get_derivatives(self.network)
+        self.network.get_derivatives()
 
     def test_get_inputs(self):
-        inputs = self.network[2][0].get_inputs(self.network, self.X)
+        inputs = self.network.data[2][0].get_inputs(self.network.data, self.X)
         self.assertTrue((inputs[0] == np.array([[1],
                                                 [2]])).all())
         self.assertTrue((inputs[1] == np.array([[3],
                                                 [4]])).all())
-        for index, neuron in enumerate(self.network[2]):
+        for index, neuron in enumerate(self.network.data[2]):
             neuron.output = index * np.array([1, 3])
-        inputs = self.network[1][0].get_inputs(self.network, self.X)
+        inputs = self.network.data[1][0].get_inputs(self.network.data, self.X)
         self.assertTrue((inputs[0] == np.array([[0],
                                                 [0]])).all())
         self.assertTrue((inputs[1] == np.array([[1],
                                                 [3]])).all())
 
     def test_update_X_tilde(self):
-        neuron = self.network[2][0]
+        neuron = self.network.data[2][0]
         neuron.update_X_tilde(self.network, self.X)
         self.assertTrue((neuron.X_tilde == np.array([[1, 1, 3],
                                                      [1, 2, 4]])).all())
 
     def test_update_output(self):
-        neuron = self.network[2][0]
+        neuron = self.network.data[2][0]
         neuron.update_X_tilde(self.network, self.X)
         neuron.update_output()
-        self.assertTrue((neuron.output == predict(neuron.X_tilde, neuron.w)).all())
+        self.assertTrue((neuron.output == predict(
+            neuron.X_tilde, neuron.w)).all())
         self.assertEqual(neuron.output.shape, (2, ))
 
     def test_output(self):
-        network.update_network(self.network, self.X)
-        self.assertEqual(type(network.output(self.network)), np.ndarray)
+        self.network.update_network(self.X)
+        self.assertEqual(type(self.network.output()), np.ndarray)
 
 
 if __name__ == '__main__':
