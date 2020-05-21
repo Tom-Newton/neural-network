7a691076be86848c129445f170a9b83e79e566dd
Thomas Newton <thomas.w.newton@gmail.com>
2020-05-21 16:44:25 +0100

    Tidy up

--- image_classifier.py
+++ image_classifier.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cupy as cp
-import matplotlib.pyplot as plt
 from network import Network, log_likelihood, calculate_confusion_matrix
 from single_neuron import SingleNeuron, Softmax
 from helper_functions import unpickle, normalise, un_transform_image
@@ -82,7 +81,7 @@ ll_test = log_likelihood(Y_test, predictions)
 print(
     f'Initial ll_train = {ll_train/n_train}, ll_test = {ll_test/(n_test)}')
 
-# w_data = cp.load('w_data.npy', allow_pickle=True)
+# w_data = np.load('cifar-10/w_data.npy', allow_pickle=True)
 # network.set_weights(w_data)
 
 network.train(X2_train, Y_train, 50)
@@ -94,44 +93,14 @@ ll_test = log_likelihood(Y_test, predictions)
 print(
     f'Final ll_train = {ll_train/n_train}, ll_test = {ll_test/(n_test)}\n')
 
-# confusion_matrix = np.round(calculate_confusion_matrix(Y_test, predictions), 2)
+try:
+    with open('cifar-10/ll_test.txt', 'r') as file:
+        old_ll_test = float(file.readline())
+except FileNotFoundError:
+    old_ll_test = -np.inf
 
-# _, ax = plt.subplots()
-# ax.set_xticks(np.arange(number_classes))
-# ax.set_yticks(np.arange(number_classes))
-# ax.set_xticklabels(list(range(number_classes)))
-# ax.set_yticklabels(list(range(number_classes)))
-# ax.set_title('Confusion matrix P(predicted class | true class)')
-# plt.xlabel('Predicted class')
-# plt.ylabel('True class')
-# for i in range(number_classes):
-#     for j in range(number_classes):
-#         ax.text(j, i, round(
-#             confusion_matrix[i, j], 3), ha='center', va='center', color='w')
-# ax.imshow(confusion_matrix)
-
-# hard_predictions = np.argmax(predictions, 1)
-# true_classes = np.argmax(Y_test, 1)
-
-# for n in range(number_classes):
-#     fig = plt.figure()
-#     fig.suptitle(f'Supposed to be {n}s but incorrectly categorised')
-#     incorrect_predictions = np.logical_and(
-#         true_classes == n, hard_predictions != n)
-#     incorrect_class = hard_predictions[incorrect_predictions]
-#     x_indices = incorrect_predictions.nonzero()[0]
-#     number_to_display = np.sum(incorrect_predictions)
-#     for l in range(number_to_display):
-#         plt.subplot(1, number_to_display, l+1)
-#         plt.title(incorrect_class[l])
-#         plt.imshow(un_transform_colour_image(images_test[x_indices[l]], (32, 32)))
-
-with open('ll_test.txt', 'r') as file:
-    old_ll_test = float(file.readline())
 if old_ll_test <= ll_test:
     print('Updating old weights')
-    cp.save('w_data.npy', network.get_weights())
-    with open('ll_test.txt', 'w+') as file:
+    np.save('cifar-10/w_data.npy', network.get_weights())
+    with open('cifar-10/ll_test.txt', 'w+') as file:
         file.write(str(ll_test))
-
-plt.show()
